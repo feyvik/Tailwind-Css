576982f3f86236f66fc504f0c3f7d2636cb832bd
favour vivian woka <vivianemma97@gmail.com>
2020-08-06 07:10:14 +0100

    Set up tailwind css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Navigation />
+
+    <div>
+      <div
+        class="bg-purple text-white sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange ..."
+      >
+        Test
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Navigation from "@/components/Navigation.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Navigation
   }
 };
 </script>
